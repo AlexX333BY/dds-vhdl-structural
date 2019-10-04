9b7172bb5a6d2a72f45df7c978bc95a3d899f730
Aliaksei <alexx333by@protonmail.com>
2019-10-04 22:25:23 +0300

    Added behavioral description of combinational element

--- StructuralVhdl/Combintaional.vhd
+++ StructuralVhdl/Combintaional.vhd
@@ -1,15 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Combintaional is
     Port ( W : in  STD_LOGIC;
            X : in  STD_LOGIC;
@@ -45,3 +36,7 @@ begin
    C3: Or2 port map (WXY, XZ, G);
 end Structural;
 
+architecture Behavioral of Combintaional is
+begin
+   G <= (W and X and Y) or (X and Z);
+end Behavioral;
